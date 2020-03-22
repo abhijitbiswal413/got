2e3a1afe60c467ec3da56e7563c230e2fd216fce
namandangi <danginaman55@gmail.com>
2020-03-22 23:29:55 +0530

    recieve data from backend and dynamically render cards

--- webapp/src/components/columnCard.js
+++ webapp/src/components/columnCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import img from './480240.png';
 import './columnCard.css';
 
@@ -9,52 +9,59 @@ import {
   Link  
 }  from 'react-router-dom';
 
-export default function ColumnCard(){
+export default class ColumnCard extends Component{
+		state ={
+			title :[],
+			id : [],
+			data :''
+		}		
+
+	  async componentDidMount() {
+	  	const response = await fetch('http://localhost:4000/api/gotb/list');
+	  	const content = await response.json();
+	  	await this.setState({data:JSON.stringify(content)});
+	 	const temp = [];
+	 	const tempid = [];
+	  	for(var i=0;i<content.length;i++){
+	  		temp[i] = content[i].name;
+	  		tempid[i] = content[i]._id;
+	  	}
+	  	this.setState({title:temp});
+	  	console.dir(this.state.title ,temp ,tempid)
+}
+/*function Card(){
+  	return(
+  		<div id="card">
+			<h3>BATTLE OF THE BASTARDS</h3>
+			<Link to="/list/id"><button>WATCH</button></Link>
+			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
+			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
+		</div>
+  		);
+  }
+*/
+	render(){
+		
 	return(
 		<>
+		
+		
+		
 		<Link to="/"><a href="#" id="back"style={{fontSize:35,marginLeft:'10rem',width:'40',padding:0}}>&#60;</a></Link>
+		{this.state.title.map((el,id)=>
+				<li key={id}>
 		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
+			<h3>{el}</h3>
 			<Link to="/list/id"><button>WATCH</button></Link>
 			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
 			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>
-		<div id="card">
-			<h3>BATTLE OF THE BASTARDS</h3>
-			<Link to="/list/id"><button>WATCH</button></Link>
-			<div id="attack" style={{backgroundImage:`url(${img})`}}></div>
-			<div id="defend" style={{backgroundImage:`url(${img})`}}></div>
-		</div>		
+		</div>	  	
+				</li>
+			)}
+  		
+		
+		
 		</>
 	);
+}
 };
