e0ff5666c78c6588ed03262d609bb905ec0e8df0
namandangi <danginaman55@gmail.com>
2020-03-24 03:42:53 +0530

    add battle count component

---
+++ webapp/src/components/battleCount.js
@@ -0,0 +1,35 @@
+import React , {Component} from 'react';
+import logo from './logo192.png';
+import img from './img192.png';
+import backdrop from './backdrop/battle.jpg';
+import './battleCount.css'
+
+import {Link}  from 'react-router-dom';
+
+export default class Count extends Component{
+	state = {
+		count:''
+	}
+	async componentDidMount(){
+		const response = await fetch('http://localhost:4000/api/gotb/count');
+		const content = await response.json();
+		await this.setState({count:(content)});
+		console.dir(content);
+	}
+	render(){
+		return(
+			<>
+			<div id="countbackbtn"><Link to="/"><a href="#"> &#60; </a></Link></div>
+			<div id="countcard" style={{backgroundImage:`url(${backdrop})`}}>  				
+			    <div className="battleprofile">
+			      <h1 style={{color:'white',fontWeight:'bold'}}>X &nbsp; B A T T L E &emsp; C O U N T &nbsp; X</h1>      
+			      <hr />   
+			     <h3 style={{color:'white',fontWeight:'bold'}}>{this.state.count}</h3>
+			     <Link to='/list'><button style={{width:200}}> WATCH &nbsp; BATTLES </button></Link>
+			     <div className="overlay"></div>
+			    </div>
+			</div>
+			</>
+			);
+	}
+}
